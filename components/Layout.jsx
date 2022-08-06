5c509fa57bbcb77b3b95005d7eb66a6b5c93c1e4
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-08-06 18:33:04 -0400

    components and about to connect SQL

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Header } from './Header'
+import { Header } from './'
 
-const Layout = ( {children }) => {
+const Layout = ({ children }) => {
     return (
         <>
             <Header />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Layout } from '../components
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import { Layout } from '../components';
+import '../styles/globals.scss';
+import 'tailwindcss/tailwind.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return(
     <Layout>
-
+      <Component {...pageProps} />
     </Layout>
-    <Component {...pageProps} />
-
   )
 }
 
